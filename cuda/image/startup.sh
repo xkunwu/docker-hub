b7d7960ef350262b2d6e40b8603f6f7df1ef4aac
Kelvin <xkun.wu@gmail.com>
2018-11-07 13:03:53 +0000

    security issues

--- cuda/image/startup.sh
+++ cuda/image/startup.sh
@@ -5,25 +5,40 @@ if [ -n "$RESOLUTION" ]; then
     sed -i "s/1280x800/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
 
+# default user should be you
+# USER=$(whoami)
 USER=${USER:-root}
-# HOME=/root
-if [ "$USER" != "root" ]; then
-    echo "* enable custom user: $USER"
-    useradd --uid 1000 --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
-    if [ -z "$PASSWORD" ]; then
-        echo "  set default password to \"123123\""
-        PASSWORD=123123
-    fi
-    HOME=/home/$USER
-    echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.vimrc,.tmux.conf} ${HOME}
-    chown -R $USER:$USER $HOME
+if [ "$USER" == "root" ]; then
+    HOME=/root
 fi
+
+# USER=${USER:-root}
+# # HOME=/root
+# if [ "$USER" != "root" ]; then
+#     echo "* enable custom user: $USER"
+#     useradd --uid 1000 --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+#     if [ -z "$PASSWORD" ]; then
+#         echo "  set default password to \"123123\""
+#         PASSWORD=123123
+#     fi
+#     HOME=/home/$USER
+#     echo "$USER:$PASSWORD" | chpasswd
+#     cp -r /root/{.vimrc,.tmux.conf} ${HOME}
+#     chown -R $USER:$USER $HOME
+# fi
+
+# this part is not necessary
 # sed -i "s|%(ENV_USER)s|$USER|" /etc/supervisor/conf.d/supervisord.conf
 # sed -i "s|%(ENV_HOME)s|$HOME|" /etc/supervisor/conf.d/supervisord.conf
 
-# clearup
-PASSWORD=
+# # clearup
+# PASSWORD=
+# VNC_PASSWORD=
+
+# must provide vnc password
+x11vnc -storepasswd /.vncpass
+chmod 400 /.vncpass
+sed -i 's/^command=x11vnc.*/& -rfbauth \/.vncpass/' /etc/supervisor/conf.d/supervisord.conf
 
 # start all the services
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- cuda/image/usr/local/bin/jupyter.sh
+++ cuda/image/usr/local/bin/jupyter.sh
@@ -1,9 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-jupyter lab \
+jupyter-lab \
     --ip=0.0.0.0 --port=8888 \
     --no-browser \
     --allow-root \
-    --NotebookApp.token="${JUPYTER_TOKEN}" \
-    --NotebookApp.password="${JUPYTER_PASSWORD}" \
-    ${HOME}
+    /workspace
