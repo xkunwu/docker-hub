120e83b02214f997f4efb9c1af89510c646e5446
Kelvin <xkun.wu@gmail.com>
2018-11-21 16:40:21 +0000

    update files

---
+++ scripts/cp_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ~/.vimrc ./cuda/image/root/.vimrc
+cp ~/.tmux.conf ./cuda/image/root/.tmux.conf
+
+cp ~/.vimrc ./base/image/root/.vimrc
+cp ~/.tmux.conf ./base/image/root/.tmux.conf
